48065a7fbd6c4d7c1488d46a6322e44b5c49851d
CraftingDragon007 <68032198+CraftingDragon007@users.noreply.github.com>
2021-01-28 21:08:22 +0100

    Projektdateien hinzufügen.

---
+++ PixelPaint/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PixelPaint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+            Screen myScreen = Screen.FromControl(this);
+            Rectangle area = myScreen.WorkingArea;
+            this.Location = new Point(0, 0);
+            this.Size = area.Size;
+            */
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void neuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Main main = new Main();
+            main.TopLevel = false;
+            panel1.Controls.Add(main);
+            main.Show();
+        }
+
+        private void öffnenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PixelPaintDateien(*.pxp)|*.pxp";
+
+            if (openFileDialog.ShowDialog().Equals(DialogResult.OK))
+            {
+                Main main = new Main();
+                main.TopLevel = false;
+                panel1.Controls.Add(main);
+                main.Show();
+                main.Open(openFileDialog.FileName);
+            }
+        }
+    }
+}
---
+++ PixelPaint/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PixelPaint
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        public static Thread MainThread = Thread.CurrentThread;
+        public static List<Thread> workers = new List<Thread>();
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
